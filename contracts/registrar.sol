57cb040a69654566e792f8c6a25df6361dbc94b1
P. Doyle <patrick.doyle126@gmail.com>
2019-01-20 22:01:20 -0600

    add

--- contracts/registrar.sol
+++ contracts/registrar.sol
@@ -45,12 +45,12 @@ contract Registrar is Owned {
 
 
      /** @dev Adds a student to our Registry. 
-      * @param _address
-      * @param _grade
-      * @param _fName
-      * @param _lName
-      * @param _email
-      * @param _className
+      * @param _address  the address  of the student being added to our registry
+      * @param _grade  The students final grade. 
+      * @param _fName The Students first name that is being added to our registry.
+      * @param _lName  The Students last name that is being added to our registry. 
+      * @param _email  The students email address 
+      * @param _className  The name of the class 
       */
     function setStudent (address _address, string memory _grade, string memory _fName, string memory _lName, string memory _email, string memory _className) onlyOwner public {
         
@@ -68,15 +68,31 @@ contract Registrar is Owned {
         studentAccts.push(_address) -1;
     }
     
+    /** @dev gets all the addresses that are in our  
+      * @return _address  the array of address that have been added to our registry. 
+      */
     function getStudents () view public returns(address[] memory){
         return studentAccts;
     }
-    
-    // creating student function
+
+     /** @dev Adds a student to our Registry. 
+      * @param _address  the address  of the student being added to our registry
+      * @return _grade  The students final grade. 
+      * @return addres  The students address. 
+      * @return _fName The Students first name that is being added to our registry.
+      * @return lName  The Students last name that is being added to our registry. 
+      * @return _email  The students email address 
+      * @return  _className  The name of the class 
+      */
     function getStudent(address _address) view public returns (string memory, address, string memory, string memory, string memory, string memory){
         return (Students[_address].grade, Students[_address].studentAddr, Students[_address].fName, Students[_address].lName, Students[_address].email, Students[_address].className);
     }
-    
+
+
+
+    /** @dev gets all the addresses that are in our  
+      * @return uint the number of students in our registry.  
+      */
     function countStudents () view public returns (uint){
         return studentAccts.length; 
     }
