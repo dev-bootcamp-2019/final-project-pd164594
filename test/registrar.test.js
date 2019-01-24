4b4c5cabaa3b7f7b4eb44046da7980664e4306eb
Pat Doyle <patdoyle@Pats-MacBook-Pro.local>
2019-01-24 08:23:58 -0600

    adding second test

--- test/registrar.test.js
+++ test/registrar.test.js
@@ -30,54 +30,27 @@ contract('Registrar', function(accounts) {
 
     })
 
-    it("should emit and event when a student is added", async() => {
+    it("should return the number of students in our registry", async() => {
         const registrar = await Registrar.deployed()
+    
+        await registrar.setStudent(alice, grade, fName, lName, email, className)
 
-        var eventEmitted = false
-
-        var event = registrar.studentInfoSet(alice, grade, fName, lName, email, className)
-        await event.watch((err, res) => {
-
-            eventEmitted = true
-        })
-
-        // const amount = web3.toWei(2, "ether")
-
-        // var aliceBalanceBefore = await web3.eth.getBalance(alice).toNumber()
-        // var bobBalanceBefore = await web3.eth.getBalance(bob).toNumber()
-
-        // await supplyChain.buyItem(sku, {from: bob, value: amount})
-
-        // var aliceBalanceAfter = await web3.eth.getBalance(alice).toNumber()
-        // var bobBalanceAfter = await web3.eth.getBalance(bob).toNumber()
-
-        // const result = await supplyChain.fetchItem.call(sku)
-
-        // assert.equal(result[3].toString(10), 1, 'the state of the item should be "Sold", which should be declared second in the State Enum')
-        // assert.equal(result[5], bob, 'the buyer address should be set bob when he purchases an item')
-        assert.equal(eventEmitted, true, 'adding an item should emit a Sold event')
-        // assert.equal(aliceBalanceAfter, aliceBalanceBefore + parseInt(price, 10), "alice's balance should be increased by the price of the item")
-        // assert.isBelow(bobBalanceAfter, bobBalanceBefore - price, "bob's balance should be reduced by more than the price of the item (including gas costs)")
+        const result = await registrar.countStudents.call()
+        
+        assert.equal(parseInt(result), 2, 'the number of students in our registry')
+   
     })
 
-    // it("should allow the seller to mark the item as shipped", async() => {
-    //     const supplyChain = await SupplyChain.deployed()
+    it("it should pause the function of a contract.", async() => {
+        const registrar = await Registrar.deployed()
+        const stopped = false
 
-    //     var eventEmitted = false
+        result =  await registrar.stopRegistry()
 
-    //     var event = supplyChain.Shipped()
-    //     await event.watch((err, res) => {
-    //         sku = res.args.sku.toString(10)
-    //         eventEmitted = true
-    //     })
+    
 
-    //     await supplyChain.shipItem(sku, {from: alice})
-
-    //     const result = await supplyChain.fetchItem.call(sku)
-
-    //     assert.equal(eventEmitted, true, 'adding an item should emit a Shipped event')
-    //     assert.equal(result[3].toString(10), 2, 'the state of the item should be "Shipped", which should be declared third in the State Enum')
-    // })
+        assert.equal(result, True, 'the state of the item should be "Shipped", which should be declared third in the State Enum')
+    })
 
     // it("should allow the buyer to mark the item as received", async() => {
     //     const supplyChain = await SupplyChain.deployed()
