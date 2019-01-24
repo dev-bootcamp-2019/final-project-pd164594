21f64d5447dd82edac2fe0db61862e334abecffd
Pat Doyle <patdoyle@Pats-MacBook-Pro.local>
2019-01-23 19:48:34 -0600

    fixing web3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+$("#transactionText").hide();
 
 
-        $("#transactionText").hide();
+var defaultAccount
+
 
         window.addEventListener('load', async () => {
           // Modern dapp browsers...
@@ -26,16 +28,18 @@
               console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
           }
       });
-        // var web3 = new Web3(new Web3.providers.HttpProvider(
-        //     'https://ropsten.infura.io/[3f152de5c51d4490b60883598c1d8418]'
-        //  ));
 
-        web3.eth.defaultAccount = web3.eth.accounts[0];
-        $("#ethaddress").html(web3.eth.defaultAccount)
+        console.log(web3.eth.defaultAccount)
+        web3.eth.getAccounts(function(error, accounts){
+          defaultAccount = accounts;
+          console.log(web3.eth.defaultAccount)
+          console.log(defaultAccount)
+          $("#ethaddress").html(web3.eth.defaultAccount)
+        })
 
 
 
-        console.log(web3.eth.defaultAccount);
+   
         // Adding the contracts ABI. //Add your contract ABI in between parenthesis. 
         const RegistrarContract = web3.eth.contract([{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x8da5cb5b"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"studentAccts","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function","signature":"0xb37d1f42"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0xf2fde38b"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor","signature":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"grade","type":"string"},{"indexed":false,"name":"studentAddr","type":"address"},{"indexed":false,"name":"fName","type":"string"},{"indexed":false,"name":"lName","type":"string"},{"indexed":false,"name":"email","type":"string"},{"indexed":false,"name":"className","type":"string"}],"name":"studentInfoSet","type":"event","signature":"0xeb01e5f67eb4012e734a88491c4ccb6a775d42e662e3d132fd1f69fea2e3057f"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event","signature":"0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_grade","type":"string"},{"name":"_fName","type":"string"},{"name":"_lName","type":"string"},{"name":"_email","type":"string"},{"name":"_className","type":"string"}],"name":"setStudent","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0xbc736646"},{"constant":true,"inputs":[],"name":"getStudents","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function","signature":"0xf1d064b3"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"getStudent","outputs":[{"name":"","type":"string"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x6b7b44d7"},{"constant":true,"inputs":[],"name":"countStudents","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x477f5a69"},{"constant":false,"inputs":[],"name":"stopRegistry","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0xc2feb650"},{"constant":false,"inputs":[],"name":"startRegistry","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x976e68a3"}])
         
@@ -87,3 +91,5 @@
         })
 
       })
+      
+     
